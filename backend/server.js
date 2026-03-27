456a107193dea7e55bdb317de3cfde0882ae652c
alofrrr <analauraoferreira1@gmail.com>
2026-03-26 23:03:14 -0300

    fix: server

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,6 @@ const { Server } = require('socket.io');
 const multer = require('multer');
 const mammoth = require('mammoth');
 const cors = require('cors');
-const path = require('path');
 const fs = require('fs');
 const Redis = require('ioredis');
 
